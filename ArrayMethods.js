240ffa5bf91b902bd3919b1bbefef134671b2c94
vigneshguru <vigneshguru134@gmail.com>
2022-03-25 16:18:50 +0530

    todo

---
+++ ArrayMethods.js
@@ -0,0 +1,54 @@
+
+const details = [
+    {
+        fname:"vignesh",
+        lname:"guru",
+        place:"ponneri"
+    },{
+        fname:"Adhitiya",
+        lname:"",
+        place:"tiruvallur"
+    },{
+        fname:"Keerthi",
+        lname:"vasan",
+        place:"ponneri"
+    },{
+        fname:"monish",
+        lname:"kumar",
+        place:"minjur"
+    },
+]
+
+//filter
+
+//const fiteredDetail = details.filter(detail=>detail.fname =="vignesh")
+
+//console.log("filtername",fiteredDetail);//output:filtername[ { fname: 'vignesh', lname: 'guru', place: 'chennai' } ]
+
+//const placeDetail = details.filter(detail=>detail.place =="ponneri")
+
+//console.log("filterplace",placeDetail);//output:filterplace[{ fname: 'vignesh', lname: 'guru', place: 'ponneri' },{ fname: 'Keerthi', lname: 'vasan', place: 'ponneri' }]
+
+
+//map
+
+// const fname = details.map(detail=>detail.fname)
+// console.log("names",fname);//output: names [ 'vignesh', 'Adhitiya', 'Keerthi', 'monish' ]
+
+// const place  = details.map(detail=>detail.place)
+// console.log("place",place);//output: place [ 'ponneri', 'tiruvallur', 'ponneri', 'minjur' ]
+
+
+//find
+
+// const nameFinding = details.find(detail=>detail.fname=="vignesh")
+// console.log("namefinding",nameFinding);//namefinding { fname: 'vignesh', lname: 'guru', place: 'ponneri' }
+
+// const findLastName = details.find(detail=>detail.lname=="kumar");
+// console.log('findall',findLastName)//findall { fname: 'monish', lname: 'kumar', place: 'minjur' }
+
+
+//foreach
+
+
+
